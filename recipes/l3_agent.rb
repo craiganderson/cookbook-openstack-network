298a6c3a49c50987e1e4863d9bedc0a259e7cabd
root <root@c108.int.ccptest.attcompute.com>
2014-08-02 00:14:47 +0000

    typo fix and restart ovs agent after ovs related changes

--- recipes/l3_agent.rb
+++ recipes/l3_agent.rb
@@ -140,8 +140,9 @@ if node['openstack']['network']['l3']['external_network_bridge'].nil? or node['o
     # If port <ext_bridge_iface> doesn't exist and corresponding interface is present - add it to <ext_bridge>
     else
       execute 'add interface to external network bridge' do
-        command "ovs-vsctl add-port #{bridge} #{patch_iface} -- add-port #{parent_bridge} #{ext_bridge_iface} -- set interface #{patch_iface} type=patch options:peer=#{ext_bridge_iface} -- set interface #{ext_bridge_iface} type=patch options:peer=#{patch_iface}"
+        command "ovs-vsctl add-port #{ext_bridge} #{patch_iface} -- add-port #{parent_bridge} #{ext_bridge_iface} -- set interface #{patch_iface} type=patch options:peer=#{ext_bridge_iface} -- set interface #{ext_bridge_iface} type=patch options:peer=#{patch_iface}"
         action :run
+        notifies :restart, 'service[neutron-plugin-openvswitch-agent]', :delayed
       end
     end
 
